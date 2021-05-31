c540fd034cda0cc616f29eea2260933292527b9a
XRarity <egger.laurin@gmail.com>
2021-05-31 14:41:38 +0200

    "init commit"

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
     res.json({ "message": "welcome to my api"})
 })
 
+app.use((req, res) => {
+    res.status(404).send('404 not found')
+})
+
 app.listen(PORT, () => {
     console.log('listening to ' + PORT)
 })
